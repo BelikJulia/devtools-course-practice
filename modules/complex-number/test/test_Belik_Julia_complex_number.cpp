50e3dc9f5be3054367261e0be45f85a2c6f593a4
BelikJulia <ybelik2015@yandex.ru>
2021-03-15 10:11:46 +0300

    Updated tests

--- modules/complex-number/test/test_Belik_Julia_complex_number.cpp
+++ modules/complex-number/test/test_Belik_Julia_complex_number.cpp
@@ -2,36 +2,47 @@
 
 #include <gtest/gtest.h>
 
+#include <tuple>
 #include "include/complex_number.h"
 
-TEST(Belik_Julia_ComplexNumberTest, Can_SetGet_Im) {
+TEST(Belik_Julia_ComplexNumberTest, Can_Copy) {
     // Arrange
-    ComplexNumber a;
+    ComplexNumber a(15.0, -42.0);
 
     // Act
-    a.setIm(42);
+    ComplexNumber b(a);
+    bool c = a == b;
 
     // Assert
-    EXPECT_EQ(42, a.getIm());
+    EXPECT_TRUE(c);
 }
 TEST(Belik_Julia_ComplexNumberTest, Conjugate_Numbers_Multiplication) {
     // Arrange
     ComplexNumber a(5, 1);
     ComplexNumber b(5, -1);
 
-    // Assert
-    EXPECT_EQ((a * b).getRe(), 26);
-    EXPECT_EQ((a * b).getIm(), 0);
-}
-TEST(Belik_Julia_ComplexNumberTest, Test_Equal) {
-    // Arrange
-    double re1 = 5;
-    double im1 = 8;
-    double re2 = 5;
-    double im2 = 8;
-    ComplexNumber a(re1, im1);
-    ComplexNumber b(re2, im2);
+    // Act
+    ComplexNumber c = a * b;
 
     // Assert
-    EXPECT_TRUE(a == b);
+    EXPECT_EQ(c.getRe(), 26);
+    EXPECT_EQ(c.getIm(), 0);
 }
+typedef testing::TestWithParam<std::tuple<double, double>> Belik_Julia_ComplexNumberTest_Param;
+TEST_P(Belik_Julia_ComplexNumberTest_Param, Test_Equal) {
+    // Arrange
+    double re = std::get<0>(GetParam());
+    double im = std::get<1>(GetParam());
+    ComplexNumber a(re, im);
+    ComplexNumber b(re, im);
+
+    // Act
+    bool c = a == b;
+
+    // Assert
+    EXPECT_TRUE(c);
+}
+INSTANTIATE_TEST_CASE_P(/**/, Belik_Julia_ComplexNumberTest_Param, testing::Combine(
+    testing::Values(-17.0, 2.0),
+    testing::Values(8.0, -4.0)
+));
