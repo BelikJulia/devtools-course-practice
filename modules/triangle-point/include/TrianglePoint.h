2e59d4f76f0618264c647acbcce8abaa7ba78168
BelikJulia <ybelik2015@yandex.ru>
2021-04-15 10:30:31 +0300

    up

--- modules/triangle-point/include/TrianglePoint.h
+++ modules/triangle-point/include/TrianglePoint.h
@@ -7,12 +7,14 @@
 
 class Triangle {
 public:
+
     Triangle(std::pair<double, double>  _a, std::pair<double, double> _b,
         std::pair<double, double> _c);
 
     bool isInside(std::pair<double, double>  x);
 
 private:
+
     std::pair<double, double>  a;
     std::pair<double, double>  b;
     std::pair<double, double>  c;
