f4b3548e228e1043150f472c7af76d57c1a1db59
BelikJulia <ybelik2015@yandex.ru>
2021-05-04 13:49:56 +0300

    lab3

---
+++ modules/deposit_calc/src/DepositCalc_app.cpp
@@ -0,0 +1,109 @@
+// Copyright 2021 Belik Julia
+
+#include <utility>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "include/DepositCalc.h"
+#include "include/DepositCalc_app.h"
+
+using std::stoi;
+
+DepositCalc_app::DepositCalc_app() : message_("") {}
+
+void DepositCalc_app::help(const char* appname, const char* message) {
+    message_ =
+        std::string(message) +
+        "This is a deposit calc application.\n\n" +
+        "Please provide arguments in the following format:\n\n" +
+
+        "  $ " + appname + " <deposit Sum> " +
+        "<day> <month> <year> <interest Rate>" +
+        " <number Of Months> <capitalization>\n\n" +
+
+        "Where deposit Sum, day, month, year," +
+        " number Of Months are integer numbers, \n" +
+        "Interest Rate is double, \n" +
+        "and capitalization is \n 0 - No capitalization \n 1 - Once a day \n" +
+        " 2 - Once a week \n 3 - Once a month \n 4 - Once a year ";
+}
+
+bool DepositCalc_app::validateNumberOfArguments(int argc, const char** argv) {
+    if (argc == 1) {
+        help(argv[0]);
+        return false;
+    }
+    else if (argc != 8) {
+        help(argv[0], "Error: Should be 8 arguments.\n\n");
+        return false;
+    }
+    return true;
+}
+
+bool DepositCalc_app::validateArguments(const char** argv,
+    std::vector<int> daysInMonths) {
+    if (std::stoi(argv[1]) < 0) {
+        help(argv[0], "Error: Sum must be positive.\n\n");
+        return false;
+    }
+    if (std::stoi(argv[3]) < 0) {
+        help(argv[0], "Error: Number of months must be positive.\n\n");
+        return false;
+    }
+    if (std::stoi(argv[2]) < 0) {
+        help(argv[0], "Error: Number of days must be positive.\n\n");
+        return false;
+    }
+    if (std::stoi(argv[5]) < 0) {
+        help(argv[0], "Error: Interest rate must be positive.\n\n");
+        return false;
+    }
+    if (std::stoi(argv[2]) <= 0 ||
+        std::stoi(argv[2]) > daysInMonths[std::stoi(argv[3]) % 12]
+        || std::stoi(argv[3]) <= 0 || std::stoi(argv[3]) > 12 ||
+        std::stoi(argv[4]) < 0) {
+        help(argv[0], "Error: Incorrect date.\n\n");
+        return false;
+    }
+    if (std::stoi(argv[7]) < 0 || std::stoi(argv[7]) > 4) {
+        help(argv[0], "Error: Wrong value.\n\n");
+        return false;
+    }
+    return true;
+}
+
+std::string DepositCalc_app::operator()(int argc, const char** argv) {
+    depositCalc depc;
+    Arguments args;
+    if (!validateNumberOfArguments(argc, argv)) {
+        return message_;
+    }
+    if (!validateArguments(argv, depc.getDaysInMonths())) {
+        return message_;
+    }
+    try {
+        args.depositSum = stoi(argv[1]);
+        args.numberOfMonths = stoi(argv[6]);
+        args.interestRate = stoi(argv[5]);
+        args.day = stoi(argv[2]);
+        args.month = stoi(argv[3]);
+        args.year = stoi(argv[4]);
+        args.capitalization = stoi(argv[7]);
+    }
+    catch (...) {
+        throw("Invalid arguments");
+    }
+    depc.setDepositSum(args.depositSum);
+    depc.setDate(args.day, args.month, args.year);
+    depc.setInterestRate(args.interestRate);
+    depc.setNumberOfMonths(args.numberOfMonths);
+    depc.setCapitalization(args.capitalization);
+    std::ostringstream stream;
+    int res;
+    depc.calculate();
+    res = depc.getResult();
+    stream << "Profitability= " << res << ".";
+    message_ = stream.str();
+    return message_;
+}
